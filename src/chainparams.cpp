040f597aec7e04131fd4d5c06c7b620af1c297cb
ilankay <ilankayesar+git@gmail.com>
2025-01-15 21:04:33 +0200

    changed consensus

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -129,21 +129,9 @@ public:
         m_is_mockable_chain = false;
 
         checkpointData = {
-            {
-                { 11111, uint256S("0x0000000069e244f73d78e8fd29ba2fd2ed618bd6fa2ee92559f542fdb26e7c1d")},
-                { 33333, uint256S("0x000000002dd5588a74784eaa7ab0507a18ad16a236e7b1ce69f00d7ddfb5d0a6")},
-                { 74000, uint256S("0x0000000000573993a3c9e41ce34471c079dcf5f52a0e824a81e7f953b8661a20")},
-                {105000, uint256S("0x00000000000291ce28027faea320c8d2b054b2e0fe44a773f3eefb151d6bdc97")},
-                {134444, uint256S("0x00000000000005b12ffd4cd315cd34ffd4a594f430ac814c91184a0d42d2b0fe")},
-                {168000, uint256S("0x000000000000099e61ea72015e79632f216fe6cb33d7899acb35b75c8303b763")},
-                {193000, uint256S("0x000000000000059f452a5f7340de6682a977387c17010ff6e6c3bd83ca8b1317")},
-                {210000, uint256S("0x000000000000048b95347e83192f69cf0366076336c639f9b7228e9ba171342e")},
-                {216116, uint256S("0x00000000000001b4f4b433e81ee46494af945cf96014816a4e2370f11b23df4e")},
-                {225430, uint256S("0x00000000000001c108384350f74090433e7fcf79a606b8e797f065b130575932")},
-                {250000, uint256S("0x000000000000003887df1f29024b06fc2200b55f8af8f35453d7be294df2d214")},
-                {279000, uint256S("0x0000000000000001ae8c72a0b0c301f67e3afca10e819efa9041e458e9bd7e40")},
-                {295000, uint256S("0x00000000000000004d9b4ef50f0f9d686fd69db2e03af35a100370c64632a983")},
-            }
+          {
+            {0, uint256S("0x0")}
+          }
         };
 
         chainTxData = ChainTxData{
@@ -222,7 +210,7 @@ public:
 
         checkpointData = {
             {
-                {546, uint256S("000000002a936ca763904c3c35fce2f3556c559c0214345d31b1bcebf76acb70")},
+                {0, uint256S("0x0")},
             }
         };
 
@@ -272,7 +260,7 @@ public:
         pchMessageStart[1] = 0xb0;
         pchMessageStart[2] = 0xb0;
         pchMessageStart[3] = 0xd0;
-        nDefaultPort = 12444;
+        nDefaultPort = 12243;
         nPruneAfterHeight = 1000;
         m_assumed_blockchain_size = 0;
         m_assumed_chain_state_size = 0;
@@ -282,6 +270,12 @@ public:
         genesis = CreateGenesisBlock(1736623623, 848471977, 0x1d00ffff, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
         assert(consensus.hashGenesisBlock == uint256S("0x00000000daa3c6f1553327b886ea5e39bcdbee886ee9ba060b6a63b27e88ad22"));
+        assert(genesis.hashMerkleRoot == uint256S("0x21d106f4d1cfd218c550112b140d31a472225d0bfbde1c533aa7d8712617279f"));
+
+
+        genesis = CreateGenesisBlock(1736545871, 3785522035, 0x1d00ffff, 1, 50 * COIN);
+        consensus.hashGenesisBlock = genesis.GetHash();
+        assert(consensus.hashGenesisBlock == uint256S("0x00000000e0b88f968e73b67010813f08a80ad686703bc52923a1d8ccab80c082"));
         assert(genesis.hashMerkleRoot == uint256S("0x21d106f4d1cfd218c550112b140d31a472225d0bfbde1c533aa7d8712617279f"));
 
         vFixedSeeds.clear(); //!< Regtest mode doesn't have any fixed seeds.
